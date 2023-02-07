0e270fbb862d15717a90f5f83b0498497515271d
yupanasenko <123364029+yupanasenko@users.noreply.github.com>
2023-02-07 15:54:00 +0300

    Update example-realpath.c

--- examples/akbase/example-realpath.c
+++ examples/akbase/example-realpath.c
@@ -7,7 +7,7 @@
     char resolved_path[1024];
 
    /* преобразование простого имени файла */
-    printf(" 1. result: %d, path: %s\n",
+    /*printf(" 1. result: %d, path: %s\n",
                       error = ak_realpath("hello-im-file.txt", resolved_path, 1024 ), resolved_path );
     if( error != ak_error_ok ) return EXIT_FAILURE;
     printf(" 2. result: %d, path: %s\n",
@@ -15,26 +15,26 @@
     if( error != ak_error_ok ) return EXIT_FAILURE;
     printf(" 3. result: %d, path: %s\n",
                              error = ak_realpath(".hello.txt", resolved_path, 1024 ), resolved_path );
-    if( error != ak_error_ok ) return EXIT_FAILURE;
+    if( error != ak_error_ok ) return EXIT_FAILURE;*/
 
    /* преобразование имени, начинающегося с / */
-    printf(" 4. result: %d, path: %s\n",
+    /*printf(" 4. result: %d, path: %s\n",
                              error = ak_realpath("/some-file", resolved_path, 1024 ), resolved_path );
     if( error != ak_error_ok ) return EXIT_FAILURE;
     printf(" 5. result: %d, path: %s\n", error =
           ak_realpath("/usr/share/doc/some-packet/readme.txt", resolved_path, 1024 ), resolved_path );
-    if( error != ak_error_ok ) return EXIT_FAILURE;
+    if( error != ak_error_ok ) return EXIT_FAILURE;*/
 
    /* преобразование имени, начинающегося с ./ */
-    printf(" 6. result: %d, path: %s\n",
+    /*printf(" 6. result: %d, path: %s\n",
                      error = ak_realpath("./current-file.txt", resolved_path, 1024 ), resolved_path );
     if( error != ak_error_ok ) return EXIT_FAILURE;
     printf(" 7. result: %d, path: %s\n", error =
                     ak_realpath("./another-special-file.docx", resolved_path, 1024 ), resolved_path );
-    if( error != ak_error_ok ) return EXIT_FAILURE;
+    if( error != ak_error_ok ) return EXIT_FAILURE;*/
 
    /* преобразование имени, начинающегося с ../ */
-    printf(" 8. result: %d, path: %s\n",
+    /*printf(" 8. result: %d, path: %s\n",
                            error = ak_realpath("../hello.txt", resolved_path, 1024 ), resolved_path );
     if( error != ak_error_ok ) return EXIT_FAILURE;
     printf(" 9. result: %d, path: %s\n",
@@ -42,7 +42,7 @@
     if( error != ak_error_ok ) return EXIT_FAILURE;
     printf("10. result: %d, path: %s\n",
                         error = ak_realpath("~/.fluxbox/menu", resolved_path, 1024 ), resolved_path );
-    if( error != ak_error_ok ) return EXIT_FAILURE;
+    if( error != ak_error_ok ) return EXIT_FAILURE;*/
 
  return EXIT_SUCCESS;
 }
